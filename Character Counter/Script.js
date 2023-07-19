049a8a823ef1df80c4dcda39ec146c8d30348cdd
ㅤㅤㅤ <64054647+catgirIz@users.noreply.github.com>
2023-07-19 19:09:30 -0400

    fix

--- Character Counter/Script.js
+++ Character Counter/Script.js
@@ -19,7 +19,7 @@ module.exports = {
         var AmtEle = document.querySelector(
           "#app > div > div.ReorderPortalContext-container > div.GameContext-container.GameContext-container-no-game.AppV2-game-context.AppV2-container-minimal-nav.AppV2-electron-windows > div.StatusContext-container.AppV2-container > div.WebAppV2-container.WebAppV2-container-has-sidebar.WebAppV2-container-minimal.WebAppV2-container-electron.WebAppV2-container-electron-windows.WebAppV2-container-has-title-bar > div.LayerContext-container > div.LayerContext-layer-item-wrapper > div > div > div.LayerContext-container.LayeredPageContent-container.LayeredPageContent-container-desktop > div.LayerContext-layer-item-wrapper > div > div.ChannelWrapper-contents.ChannelWrapper-contents-sidebar-collapsed > div > div > div.ChatChannelInput-container > div.Editor-container.Editor-container-type-simple.ChatChannelInput-editor > div.SlateEditorContext-container.SlateEditor-container.SlateEditor-container-type-simple.needsclick > div.needsclick.SlateEditor-editor.SlateEditor-editor-has-max-height > div > span > span > span"
         );
-        var Amt = AmtEle.textContent.length;
+        var Amt = AmtEle.textContent.length+1;
         var Check = AmtEle.firstChild.nodeType;
         if (Check == 1) {
           Amt = 0;
