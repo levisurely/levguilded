6a5649fe3ac3d6add4522dcb2ebe9fa52c6aada2
ㅤㅤㅤ <64054647+catgirIz@users.noreply.github.com>
2023-07-23 16:33:54 -0400

    update

--- CSS Editor/Script.js
+++ CSS Editor/Script.js
@@ -1,19 +1,37 @@
-const ver = "0.0.1";
+const ver = "0.0.3";
 var live = false;
 
 module.exports = {
   load() {
-    //console.log("LOAD");
+    if (!document.getElementById("outputcss")) {
+      var outputDiv = document.createElement("div");
+      outputDiv.id = "outputcss";
+      document.body.appendChild(outputDiv);
+      if (localStorage.getItem("outputcss")) {
+        outputDiv.innerHTML =
+          "<style>" + localStorage.getItem("outputcss") + "</style>";
+      }
+    }
     btn();
     var stnbtn = document.querySelector(
       "#app > div > div.ReorderPortalContext-container > div.GameContext-container.GameContext-container-no-game.AppV2-game-context.AppV2-container-minimal-nav.AppV2-electron-windows > div.StatusContext-container.AppV2-container > div.WebAppV2-container.WebAppV2-container-has-sidebar.WebAppV2-container-minimal.WebAppV2-container-electron.WebAppV2-container-electron-windows.WebAppV2-container-has-title-bar > div.WebAppSidebar-container.WebAppV2-sidebar > div.NavV4Footer-container.WebAppSidebar-footer > div > div > div.NavbarNavIcons-container.NavbarNavIcons-container-spacing-sm.NavV4Footer-nav-icons > div.NavbarNavIcons-user-settings"
     );
     stnbtn.addEventListener("click", function () {
-      //console.log("SETTINGS");
       setTimeout(function () {
         btn();
-      }, 2000);
+      }, 1000);
     });
+  },
+  init() {
+    if (!document.getElementById("outputcss")) {
+      var outputDiv = document.createElement("div");
+      outputDiv.id = "outputcss";
+      document.body.appendChild(outputDiv);
+      if (localStorage.getItem("outputcss")) {
+        outputDiv.innerHTML =
+          "<style>" + localStorage.getItem("outputcss") + "</style>";
+      }
+    }
   },
   unload() {
     if (document.getElementById("cssbtn")) {
@@ -26,12 +44,8 @@ module.exports = {
 };
 
 async function btn() {
-  //console.log("BTN");
   if (document.getElementById("cssbtn")) {
     document.getElementById("cssbtn").remove();
-  }
-  if (document.getElementById("outputcss")) {
-    document.getElementById("outputcss").remove();
   }
   var cool = await document.querySelector(
     "#app > div > div.ReorderPortalContext-container > div.GameContext-container.GameContext-container-no-game.AppV2-game-context.AppV2-container-minimal-nav.AppV2-electron-windows > div.OverlayStackProvider-container > div.PortalTarget-container.OverlayStackProvider-portal-target > span > div.StatusContext-container.Overlay-status-context > div > div.StatusContext-container.ModalV2-modal-content.ModalV2-modal-content-scrollable > div > div > div > div.OptionsMenu-options-control > div.DesktopOptionsControl-container.OptionsMenu-desktop-options-control > div > div:nth-child(3) > div.PersistentActionMenuSection-sections-wrapper.PersistentActionMenu-menu-section.DesktopOptionsControl-persistent-action-menu-section > div:nth-child(3)"
@@ -43,9 +57,12 @@ async function btn() {
   const clone = cool.cloneNode(true);
   clone.id = "cssbtn";
   par.appendChild(clone);
-  document.querySelector(
+  var newbtn = document.querySelector(
     "#app > div > div.ReorderPortalContext-container > div.GameContext-container.GameContext-container-no-game.AppV2-game-context.AppV2-container-minimal-nav.AppV2-electron-windows > div.OverlayStackProvider-container > div.PortalTarget-container.OverlayStackProvider-portal-target > span > div.StatusContext-container.Overlay-status-context > div > div.StatusContext-container.ModalV2-modal-content.ModalV2-modal-content-scrollable > div > div > div > div.OptionsMenu-options-control > div.DesktopOptionsControl-container.OptionsMenu-desktop-options-control > div > div:nth-child(3) > div.PersistentActionMenuSection-sections-wrapper.PersistentActionMenu-menu-section.DesktopOptionsControl-persistent-action-menu-section > div:nth-child(4) > span > div > div"
-  ).innerHTML = "CSS Editor";
+  )
+
+  newbtn.parentNode.setAttribute("data-name", "lgcssbtn");
+  newbtn.innerHTML = "CSS Editor";
 
   clone.addEventListener("mouseenter", function () {
     clone.style.backgroundColor = "rgba(255, 255, 255, 0.075)";
@@ -54,7 +71,7 @@ async function btn() {
     clone.style.backgroundColor = "";
   });
   clone.addEventListener("click", function () {
-    clone.style.backgroundColor = "rgba(255, 255, 255, 0.075)";
+
     var slide = document.querySelector(
       "#app > div > div.ReorderPortalContext-container > div.GameContext-container.GameContext-container-no-game.AppV2-game-context.AppV2-container-minimal-nav.AppV2-electron-windows > div.OverlayStackProvider-container > div.PortalTarget-container.OverlayStackProvider-portal-target > span > div.StatusContext-container.Overlay-status-context > div > div.StatusContext-container.ModalV2-modal-content.ModalV2-modal-content-scrollable > div > div > div > div.StatusContext-container.OptionsMenu-option-content.OptionsMenu-option-content-thick-scrollbar > div > div"
     );
@@ -69,6 +86,10 @@ async function btn() {
       var outputDiv = document.createElement("div");
       outputDiv.id = "outputcss";
       document.body.appendChild(outputDiv);
+      if (localStorage.getItem("outputcss")) {
+        outputDiv.innerHTML =
+          "<style>" + localStorage.getItem("outputcss") + "</style>";
+      }
     }
 
     // Create CSS textarea
@@ -137,22 +158,23 @@ async function btn() {
         document.getElementById("Live").style.backgroundColor =
           "rgba(0, 128, 0, 0.5)";
       }
-      //console.log("LIVE", live);
     });
 
     // Clear the CSS from the editor to the output div
     document.getElementById("Clear").addEventListener("click", function () {
-      //console.log(document.getElementById("css-editor").value);
       document.getElementById("css-editor").value = "";
       document.getElementById("outputcss").innerHTML = "<style></style>";
-      //console.log("CLEAR");
+      localStorage.setItem("outputcss", "");
     });
 
     // Apply the CSS from the editor to the output div
     document.getElementById("apply").addEventListener("click", function () {
+      localStorage.setItem(
+        "outputcss",
+        `${document.getElementById("css-editor").value}`
+      );
       document.getElementById("outputcss").innerHTML =
         "<style>" + document.getElementById("css-editor").value + "</style>";
-      //console.log("APPLY");
     });
 
     // Update the output div in real-time as the user types in the editor
@@ -160,14 +182,16 @@ async function btn() {
       .getElementById("css-editor")
       .addEventListener("input", function () {
         if (live == true) {
+          localStorage.setItem(
+            "outputcss",
+            `${document.getElementById("css-editor").value}`
+          );
           document.getElementById("outputcss").innerHTML =
             "<style>" +
             document.getElementById("css-editor").value +
             "</style>";
-          //console.log("CHANEGD!");
         }
       });
-
     if (
       document.getElementById("outputcss") &&
       document.getElementById("outputcss").firstChild &&
